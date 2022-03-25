0b4a36822731f07770bf30a85b80c481e2e2c46c
Stefan Andrei <andreistefanciprian@gmail.com>
2022-03-26 08:28:45 +1030

    use nlargest method

--- topX.py
+++ topX.py
@@ -1,5 +1,6 @@
 
-import sys, os
+import sys, os, time
+from heapq import nlargest
 
 class ExtractHighestNumbers:
 
@@ -14,6 +15,16 @@ class ExtractHighestNumbers:
         self._all_numbers = self._extract_numbers_from_file()
         self.top_x_numbers = self._get_highest_numbers()
 
+    def timing_val(func):
+        def wrapper(*arg, **kw):
+            t1 = time.time()
+            result = func(*arg, **kw)
+            total_time = time.time() - t1
+            print(f'{func.__name__} ran in {total_time} seconds.')
+            return result
+        return wrapper
+
+    @timing_val
     def _extract_numbers_from_file(self):
         """
         Populate all_numbers list witth all the numbers from file.
@@ -31,14 +42,19 @@ class ExtractHighestNumbers:
             return list(set(numbers))    # return list with unique numbers
         else:
             raise Exception(f'{self.numbers_file} does not exist!')
-        
+
+    @timing_val   
     def _get_highest_numbers(self):
         """
         Get highest X numbers from list of all numbers.
         """
 
-        self._all_numbers.sort(reverse=True)
-        return self._all_numbers[:self.highest_number_count]
+        # slower 1
+        # self._all_numbers.sort(reverse=True)
+        # return self._all_numbers[:self.highest_number_count]
+
+        return nlargest(self.highest_number_count, self._all_numbers)
+        
 
 
 if __name__ == "__main__":
